5ef72d0e013168e5abce55c6ff49ebac327e1761
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-02-22 20:45:38 +0700

    bật position_smoothing cho camera, làm tròn vị trí của player và camera khi di chuyển.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,9 +1,16 @@
 extends CharacterBody2D
 
+@export var speed: float = 160.0
+@export var camera_2d: Camera2D
+
 func _physics_process(delta: float) -> void:
-	velocity = Vector2(
+	var input_vec = Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		Input.get_action_strength("move_down") - Input.get_action_strength("move_up"),
-	) * 60.0
-	
+		Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	)
+
+	velocity = input_vec * speed
 	move_and_slide()
+
+	global_position = global_position.round()
+	camera_2d.global_position = global_position
