37bc48cc4ee8d2cc996d19de1e73b494c4f9aa54
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-02-24 19:42:38 +0700

    Cập nhập file .gitignore, Thêm vào animation di chuyển cho player, cập nhập sprite tile mới, code chức năng animation cho player.

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
-@export var speed: float = 160.0
+@export var speed: float = 80.0
 @export var camera_2d: Camera2D
+
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
+var last_direction: String = "down"
 
 func _physics_process(delta: float) -> void:
 	var input_vec = Vector2(
@@ -11,6 +15,28 @@ func _physics_process(delta: float) -> void:
 
 	velocity = input_vec * speed
 	move_and_slide()
+
+	if input_vec != Vector2.ZERO:
+		# ƯU TIÊN DI CHUYỂN NGANG
+		if input_vec.x != 0:
+			sprite.play("move_side")
+			sprite.flip_h = input_vec.x < 0
+			last_direction = "side"
+		else:
+			if input_vec.y > 0:
+				sprite.play("move_down")
+				last_direction = "down"
+			else:
+				sprite.play("move_up")
+				last_direction = "up"
+	else:
+		# Idle theo hướng cuối
+		if last_direction == "side":
+			sprite.play("idle_side")
+		elif last_direction == "up":
+			sprite.play("idle_up")
+		else:
+			sprite.play("idle_down")
 
 	global_position = global_position.round()
 	camera_2d.global_position = global_position
