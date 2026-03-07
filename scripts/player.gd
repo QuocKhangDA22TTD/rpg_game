3f12ae12cf14b30745f826594dc0f4e766017592
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-03-07 15:56:45 +0700

    Sửa đổi code player.gd, tách input, movement và animation ra thành 3 hàm riêng.

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,42 +2,46 @@ extends CharacterBody2D
 
 @export var speed: float = 80.0
 @export var camera_2d: Camera2D
-
 @export var animation_player: AnimationPlayer
 @export var sprite_2d: Sprite2D
 
 var last_direction: String = "down"
+var input_vector := Vector2.ZERO
 
 func _physics_process(delta: float) -> void:
-	var input_vec = Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = _get_input_vector()
+	
+	velocity = input_vector * speed
+	move_and_slide()
+	
+	_update_animation_and_direction()
+	
+	global_position = global_position.round()
+	camera_2d.global_position = global_position
+
+
+func _get_input_vector():
+	return Vector2(
+		Input.get_axis("ui_left", "ui_right"),
+		Input.get_axis("ui_up", "ui_down")
 	)
 
-	velocity = input_vec * speed
-	move_and_slide()
 
-	if input_vec != Vector2.ZERO:
-		# ƯU TIÊN DI CHUYỂN NGANG
-		if input_vec.x != 0:
+func _update_animation_and_direction():
+	if input_vector != Vector2.ZERO:
+		if input_vector.x != 0:   # ngang mạnh hơn dọc
 			animation_player.play("move_side")
-			sprite_2d.flip_h = input_vec.x < 0
+			sprite_2d.flip_h = input_vector.x < 0
 			last_direction = "side"
 		else:
-			if input_vec.y > 0:
+			if input_vector.y > 0:
 				animation_player.play("move_down")
 				last_direction = "down"
 			else:
 				animation_player.play("move_up")
 				last_direction = "up"
 	else:
-		# Idle theo hướng cuối
-		if last_direction == "side":
-			animation_player.play("idle_side")
-		elif last_direction == "up":
-			animation_player.play("idle_up")
-		else:
-			animation_player.play("idle_down")
-
-	global_position = global_position.round()
-	camera_2d.global_position = global_position
+		match last_direction:
+			"side":   animation_player.play("idle_side")
+			"up":     animation_player.play("idle_up")
+			_:        animation_player.play("idle_down")
