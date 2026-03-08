ebce07afacd72e26ea03106b4fe2b65bcf5b3be7
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-03-08 16:21:47 +0700

    Đổi tên scene inventory thành inventory_ui, tạo script inventory_ui.

---
+++ scripts/inventory_ui.gd
@@ -0,0 +1,25 @@
+extends Control
+
+@export var inventory : Inventory
+@export var slot_scene : PackedScene
+
+@export var grid: GridContainer
+
+var slot_nodes = []
+
+func _ready():
+	for i in inventory.size:
+		var slot = slot_scene.instantiate()
+		grid.add_child(slot)
+	
+		slot.slot = inventory.slots[i]
+		slot_nodes.append(slot)
+		
+	inventory.inventory_changed.connect(update_inventory)
+	
+	update_inventory()
+
+
+func update_inventory():
+	for slot in slot_nodes:
+		slot.update_ui()
