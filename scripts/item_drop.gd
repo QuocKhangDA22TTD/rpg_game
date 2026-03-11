4379c99bb3eb0215104839ecf07b5178a16100aa
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-03-11 12:31:30 +0700

    Cập nhập script item_drop để cho scene có thể hiển thị sprite vật phẩm, tạo một instance của scene item_drop vào scene demo để test.

--- scripts/item_drop.gd
+++ scripts/item_drop.gd
@@ -2,3 +2,10 @@ extends Node2D
 
 @export var item : ItemData
 @export var amount : int = 1
+
+@onready var sprite = $Sprite2D
+@onready var area = $Area2D
+
+func _ready():
+	if item and item.icon:
+		sprite.texture = item.icon
