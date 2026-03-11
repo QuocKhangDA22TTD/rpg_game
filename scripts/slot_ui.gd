00ac7caa915d12a7be8abedf95c7510fb7bddc32
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-03-11 16:14:13 +0700

    Cập nhật chức năng drag/drop item trong script slot_ui, tạo thêm 2 instance của scene item_drop trong scene demo để test.

--- scripts/slot_ui.gd
+++ scripts/slot_ui.gd
@@ -17,3 +17,32 @@ func update_ui():
 		amount.text = str(slot.amount)
 	else:
 		amount.text = ""
+
+
+func _get_drag_data(pos):
+	if slot.is_empty():
+		return
+	
+	var preview = TextureRect.new()
+	preview.texture = slot.item.icon
+	set_drag_preview(preview)
+	
+	return self
+
+
+func _can_drop_data(pos, data):
+	return data is Control and data.has_method("update_ui") 
+
+
+func _drop_data(pos, data):
+	var temp_item = slot.item
+	var temp_amount = slot.amount
+	
+	slot.item = data.slot.item
+	slot.amount = data.slot.amount
+	
+	data.slot.item = temp_item
+	data.slot.amount = temp_amount
+	
+	update_ui()
+	data.update_ui()
