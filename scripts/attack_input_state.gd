05f630b071ee8de1f1183002914326b5cbb179f5
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-04-07 14:46:42 +0700

    Tạo script attack_input_state để quản lý input kích hoạt tấn công.

---
+++ scripts/attack_input_state.gd
@@ -0,0 +1,5 @@
+class_name AttackInputState
+
+var pressed: bool           # khi nút tấn công đang được giữ
+var just_pressed: bool      # khi nút tấn công vừa được nhấn
+var just_released: bool     # khi nút tấn công vừa được thả
