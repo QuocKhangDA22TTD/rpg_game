d5d4d2c245a7a159e6ef073dd8a78048f4a500e4
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-04-07 09:12:36 +0700

    Thêm thuộc tính vào script weapon_data.

--- scripts/weapon_data.gd
+++ scripts/weapon_data.gd
@@ -2,16 +2,32 @@
 extends ItemData
 class_name WeaponData
 
-# Sát thương của vũ khí
-@export var damage: float = 10.0
-# Texture vũ khí khi trang bị
-@export var weapon_texture: Texture2D
+@export var damage: float = 10.0		# Sát thương của vũ khí
+@export var weapon_texture: Texture2D	# Texture vũ khí khi trang bị
+@export var attack_speed: float = 1.0   # số đòn/giây
+@export var range: float = 50.0         # tầm đánh
+@export var crit_chance: float = 0.1    # % chí mạng
+@export var crit_damage: float = 1.5    # Tỷ lệ chí mạng
 
-func _init(id = "", name = "", icon = null, weapon_texture = null, damage = 10.0):
-	self.id = id
-	self.name = name
-	self.icon = icon
-	self.weapon_texture = weapon_texture
-	self.damage = damage
-	self.type = ItemType.WEAPON
-	self.max_stack = 1
+@export var knockback: float = 0.0		# Lực đẩy lùi khi đánh trúng kẻ địch
+@export var stun_time: float = 0.0		# Thời gian làm choáng kẻ địch khi đánh trúng
+@export var status_effect: String = ""	# Hiệu ứng trạng thái đặc biệt khi đánh trúng kẻ địch
+
+@export var mana_cost: float = 0.0 		# Lượng mana tiêu hao khi sử dụng vũ khí
+
+@export var projectile_scene: PackedScene		# scene vật thể bay (dành cho vũ khí tầm xa)
+@export var projectile_speed: float = 300.0		# Tốc độ bay của vật thể (dành cho vũ khí tầm xa)
+
+@export var hit_effect: PackedScene		# Hiệu ứng khi đánh trúng kẻ địch
+@export var attack_sound: AudioStream	# Âm thanh khi tấn công với vũ khí
+
+@export var strength_scaling: float = 1.0	# Tỷ lệ ảnh hưởng của sức mạnh player lên sát thương
+@export var agility_scaling: float = 0.0	# Tỷ lệ ảnh hưởng của nhanh nhẹn player lên sát thương
+
+enum WeaponType {
+    MELEE,
+    RANGED,
+    MAGIC
+}
+
+@export var weapon_type: WeaponType
