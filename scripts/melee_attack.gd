f499eae69c00331dc7d9aebaa743d1ea35c5f80e
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-04-07 15:10:32 +0700

    thêm hàm handle_input vào script melee_attack để phát hiện sự kiện tấn công để kích hoạt hàm execute xử lý logic.

--- scripts/melee_attack.gd
+++ scripts/melee_attack.gd
@@ -3,3 +3,8 @@ class_name MeleeAttack
 
 func execute(user, weapon_data):
     print("Melee attack")
+
+
+func handle_input(user, weapon_data, input_state):
+    if input_state.just_pressed:
+        execute(user, weapon_data)
