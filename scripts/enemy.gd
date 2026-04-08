46ce566b51745825758b9e8cb2ba1305cebaf2eb
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-04-08 14:54:28 +0700

    Thêm các phương thức xử lý cơ bản vào script enemy.

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,1 +1,21 @@
 extends CharacterBody2D
+
+@export var speed: float = 50
+@export var max_hp: int = 10
+@export var damage: int = 1
+
+var hp: int
+
+func _ready():
+    hp = max_hp
+
+func _physics_process(delta):
+    move_and_slide()
+
+func take_damage(amount):
+    hp -= amount
+    if hp <= 0:
+        die()
+
+func die():
+    queue_free()
