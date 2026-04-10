078929270f7557e3e06e61fc8f3d2f81a352353c
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-04-10 19:15:42 +0700

    Thay đổi script enemy theo cấu trúc state. Thay đổi scene enemy.

--- scripts/enemy/enemy.gd
+++ scripts/enemy/enemy.gd
@@ -1,21 +1,53 @@
 extends CharacterBody2D
 
-@export var speed: float = 50
-@export var max_hp: int = 10
-@export var damage: int = 1
+@export var data: EnemyData
+@export var states: Array[EnemyState]
+@export var sprite_2d: Sprite2D
+@export var animation_player: AnimationPlayer
 
+var current_state: EnemyState
 var hp: int
+var speed: float
 
 func _ready():
-	hp = max_hp
+	hp = data.max_hp
+	speed = data.speed
+	
+	# GÁN TEXTURE
+	sprite_2d.texture = data.texture
+	
+	# GÁN ANIMATION
+	if data.animation_library:
+		animation_player.add_animation_library("default", data.animation_library)
+		
+	play_anim("idle")
+
+	# bắt đầu state đầu tiên
+	if states.size() > 0:
+		change_state(states[0])
+
 
 func _physics_process(delta):
+	if current_state:
+		current_state.update(self, delta)
+
 	move_and_slide()
 
-func take_damage(amount):
-	hp -= amount
-	if hp <= 0:
-		die()
 
-func die():
-	queue_free()
+func change_state(new_state):
+	if current_state:
+		current_state.exit(self)
+
+	current_state = new_state
+
+	if current_state:
+		current_state.enter(self)
+
+
+func play_anim(name):
+	if animation_player.has_animation(name):
+		animation_player.play(name)
+
+
+func get_player():
+	return get_tree().get_first_node_in_group("player")
