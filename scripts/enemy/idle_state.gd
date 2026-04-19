37801f62146c5df6642742834c4d1f32759b7136
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-04-19 20:23:34 +0700

    Cập nhập xử lý chuyển sang state chase khi phát hiện player hoặc state patrol khi thời gian ở trạng thái idle trong script idle_state quá 2 giây.

--- scripts/enemy/idle_state.gd
+++ scripts/enemy/idle_state.gd
@@ -1,7 +1,6 @@
 extends EnemyState
 class_name IdleState
 
-@export var next_state: EnemyState
 @export var wait_time: float = 2.0
 
 var timer := 0.0
@@ -14,5 +13,16 @@ func update(enemy, delta):
 	timer += delta
 	enemy.velocity = Vector2.ZERO
 
-	if timer >= wait_time and next_state:
-		enemy.change_state(next_state)
+	var distance_to_player = enemy.distance_to_player()
+
+	if distance_to_player != null and distance_to_player < 100:
+		var chase = enemy.get_state(ChaseState)
+		if chase:
+			enemy.change_state(chase)
+			return
+	
+	if timer >= wait_time:
+		var patrol = enemy.get_state(PatrolState)
+		if patrol:
+			enemy.change_state(patrol)
+			return
