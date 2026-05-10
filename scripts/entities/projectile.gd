e6fc34168e750d2de2b546a9292136f5ecbee7f5
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-05-10 17:32:23 +0700

    Tạo script projectile và attach vào scene projectile.

---
+++ scripts/entities/projectile.gd
@@ -0,0 +1,15 @@
+extends Area2D
+
+var speed: float = 0.0
+var direction: Vector2 = Vector2.ZERO
+
+func _physics_process(delta: float) -> void:
+	# Di chuyển projectile về phía trước
+	var velocity = direction * speed
+	position += velocity * delta
+
+# Xử lý va chạm với kẻ địch
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy") and body.has_method("take_damage"):
+		body.take_damage(2.0, self) # Gọi hàm take_damage trên kẻ địch, truyền vào lượng sát thương
+		queue_free()  # Xóa projectile sau khi va chạm
