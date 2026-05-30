6d9663d2cd45a16234e9ff3f759a5c44477a2448
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-05-30 17:44:39 +0700

    Cập nhập patrol_state: - Thay đổi cách tạo ngẫu nhiên mục tiêu để enemy di chuyển. - Sử dụng NavigationAgent2D để điều hướng di chuyển patrol cho enemy linh hoạt hơn. - Tạo các biến thuộc tính để tham chiếu tới các biến thuộc tính của enemy.data( class EnemyData).

--- scripts/entities/enemies/common_state/patrol_state.gd
+++ scripts/entities/enemies/common_state/patrol_state.gd
@@ -1,60 +1,58 @@
 extends EnemyState
 class_name PatrolState
 
-@export var speed: float = 40.0
-@export var radius: float = 120.0
-@export var arrive_threshold: float = 5.0
-@export var wait_time: float = 1.5
-
-var origin := Vector2.ZERO
-var target := Vector2.ZERO
-var wait_timer := 0.0
-var is_waiting := false
+var patrol_radius: float # bán kính mà kẻ địch sẽ di chuyển xung quanh
+var max_patrol_duration: float # Thời gian tối đa cho một lượt đi tuần
+var chase_distance: float # Khoảng cách mà kẻ địch sẽ bắt đầu đuổi theo người chơi
+var rng := RandomNumberGenerator.new() # bộ sinh số ngẫu nhiên để chọn mục tiêu tuần tra ngẫu nhiên
+var patrol_timer: float = 0.0 # Biến đếm thời gian
 
 func enter(enemy):
-	origin = enemy.global_position
-	_pick_new_target()
-	is_waiting = false
-	wait_timer = 0.0
-	enemy.play_anim("patrol")
+	rng.randomize() # khởi tạo bộ sinh số ngẫu nhiên
+
+	# Lấy thông số từ EnemyData để sử dụng trong quá trình tuần tra
+	patrol_radius = enemy.data.patrol_radius
+	max_patrol_duration = enemy.data.max_patrol_duration
+	chase_distance = enemy.data.chase_distance
+
+	_move_to_random_target(enemy) # gọi hàm để chọn một mục tiêu ngẫu nhiên
+
+	enemy.play_anim("patrol") # phát animation đi bộ
+
 
 func update(enemy, delta):
-	var distance_to_player = enemy.distance_to_player()
+	var distance_to_player = enemy.distance_to_player() # tính khoảng cách đến người chơi
 
-	# Ưu tiên chase
-	if distance_to_player != null and distance_to_player < 100:
+	# Nếu người chơi ở trong khoảng cách đuổi theo, chuyển sang trạng thái Chase
+	if distance_to_player != null and distance_to_player < chase_distance:
 		var chase = enemy.get_state(ChaseState)
 		if chase:
 			enemy.change_state(chase)
-			return
+
+	patrol_timer -= delta # giảm timer theo thời gian
+
+	# Nếu timer hết hoặc đã đi đến đích, chọn một mục tiêu mới
+	if patrol_timer <= 0 or enemy.navigation_agent_2d.is_navigation_finished():
+		var idle = enemy.get_state(IdleState)
+		if idle:
+			enemy.change_state(idle)
+		return
 	
-	if enemy.animation_player.current_animation_position < 0.2:
-		enemy.velocity = Vector2.ZERO
-		return
+	# di chuyển đến vị trí tiếp theo trên đường đi
+	var next_path_pos: Vector2 = enemy.navigation_agent_2d.get_next_path_position()
+	enemy.desired_velocity = enemy.global_position.direction_to(next_path_pos) * enemy.speed
 
-	# Nếu đang đứng chờ
-	if is_waiting:
-		enemy.velocity = Vector2.ZERO
-		wait_timer += delta
 
-		if wait_timer >= wait_time:
-			is_waiting = false
-			_pick_new_target()
-			enemy.play_anim("patrol")
-		return
+func _move_to_random_target(enemy):
+	# chọn một hướng ngẫu nhiên và một khoảng cách ngẫu nhiên
+	var random_direction: Vector2 = Vector2.LEFT.rotated(rng.randf_range(0, TAU))
+	var random_distance: float = rng.randf_range(0, patrol_radius)
 
-	# Di chuyển tới target
-	var direction = (target - enemy.global_position).normalized()
-	enemy.velocity = direction * speed
+	# tính toán vị trí cần di chuyển đến
+	var target: Vector2 = enemy.global_position + (random_direction * random_distance)
 
-	# Đến nơi → đứng chờ
-	if enemy.global_position.distance_to(target) <= arrive_threshold:
-		is_waiting = true
-		wait_timer = 0.0
-		enemy.play_anim("idle")
+	# giao mục tiêu cho navigation agent để di chuyển
+	enemy.navigation_agent_2d.target_position = target
 
-func _pick_new_target():
-	target = origin + Vector2(
-		randf_range(-radius, radius),
-		randf_range(-radius, radius)
-	)
+	# reset timer
+	patrol_timer = max_patrol_duration
