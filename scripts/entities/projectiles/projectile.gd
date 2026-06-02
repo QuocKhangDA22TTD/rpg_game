20526b5e2d0b66d6426c6da2c82ffbf56f093776
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-02 22:34:25 +0700

    Xóa signal body_entered trong projectile và thay thế bằng signal area_entered để có thể va chạm với hurtbox.

--- scripts/entities/projectiles/projectile.gd
+++ scripts/entities/projectiles/projectile.gd
@@ -19,8 +19,8 @@ func _physics_process(delta: float) -> void:
 	if exist_timer >= max_exist_time:
 		queue_free()  # Xóa projectile nếu đã tồn tại quá lâu
 
-# Xử lý va chạm với kẻ địch
-func _on_body_entered(body: Node2D) -> void:
-	if body.is_in_group("enemy") and body.has_method("take_damage") and speed != 0.0:
-		body.take_damage(2.0, self) # Gọi hàm take_damage trên kẻ địch, truyền vào lượng sát thương
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("enemy") and area.has_method("take_damage") and speed != 0.0:
+		area.take_damage(2.0, self) # Gọi hàm take_damage trên kẻ địch, truyền vào lượng sát thương
 		queue_free()  # Xóa projectile sau khi va chạm
