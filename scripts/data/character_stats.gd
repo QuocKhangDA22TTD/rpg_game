9e47d8399a0cc83f195db0838038335d7d537df3
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-05 13:13:55 +0700

    Cập nhập script character_stats, thay đổi giá trị biến export max_health và current_health.

--- scripts/data/character_stats.gd
+++ scripts/data/character_stats.gd
@@ -2,8 +2,8 @@ extends Resource
 
 class_name CharacterStats
 
-@export var max_health: float = 10.0
-@export var current_health: float = 10.0 :
+@export var max_health: float = 100.0
+@export var current_health: float = 100.0 :
 	set(value):
 		current_health = clamp(value, 0, max_health)
 		emit_changed() # Thông báo khi máu thay đổi (để UI cập nhật)
