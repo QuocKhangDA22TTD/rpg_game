0e43bcc494454f49fedb3130c9f229f447a8e87d
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-05 14:14:27 +0700

    Thêm các biến export và enum vào script magic_weapon_data.

--- scripts/data/magic_weapon_data.gd
+++ scripts/data/magic_weapon_data.gd
@@ -1,2 +1,12 @@
 extends WeaponData
 class_name MagicWeaponData
+
+enum CastType {
+    PROJECTILE,
+    AREA_EFFECT,
+    SELF_BUFF
+}
+
+@export var mana_cost: float = 10.0 # lượng mana tiêu thụ mỗi lần tấn công
+@export var cast_type: CastType = CastType.PROJECTILE # loại phép thuật
+@export var spell_scene: PackedScene # cảnh của phép thuật (dùng để tạo instance khi tấn công)
