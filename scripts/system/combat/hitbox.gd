1a8fef04dfbeec41ad0738a6d66ab247400ec280
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-07 16:16:05 +0700

    Cập nhập script hitbox. Sửa lại điều kiện kiểm tra class Hurtbox thày vì kiểm tra group.

--- scripts/system/combat/hitbox.gd
+++ scripts/system/combat/hitbox.gd
@@ -1,24 +1,21 @@
 extends Area2D
 class_name Hitbox
 
-signal hit_hurtbox(hurtbox: Area2D) # Tín hiệu phát ra khi hitbox va chạm với một hurtbox
+signal hit_hurtbox(hurtbox: Hurtbox) # Signal này sẽ được phát khi hitbox va chạm với một hurtbox
 
-# Danh sách để tránh hit nhiều lần trong cùng 1 lần tấn công
-var hit_entities: Array = []
+var hit_entities: Array = [] # Mảng để lưu các hurtbox đã bị hit trong lần va chạm hiện tại
 
 func _ready() -> void:
-	area_entered.connect(_on_area_entered)
+	area_entered.connect(_on_area_entered) # Kết nối signal area_entered để phát hiện va chạm với hurtbox
 
 func _on_area_entered(area: Node2D) -> void:
-	# Tránh hit cùng entity nhiều lần
 	if area in hit_entities:
-		return
+		return # Tránh hit cùng entity nhiều lần
 	
-	# Detect enemy hoặc player hurtbox
-	if area.is_in_group("enemy") or area.is_in_group("player"):
-		hit_entities.append(area)
-		hit_hurtbox.emit(area)
+	# Chỉ phát hiện hurtbox
+	if area is Hurtbox:
+		hit_entities.append(area) # Thêm hurtbox vào danh sách đã hit để tránh hit lại trong cùng một lần va chạm
+		hit_hurtbox.emit(area) # Phát signal với hurtbox đã bị hit để các hệ thống khác có thể xử lý sát thương
 
-# Reset danh sách khi animation tấn công kết thúc
 func reset_hit_list() -> void:
 	hit_entities.clear()
