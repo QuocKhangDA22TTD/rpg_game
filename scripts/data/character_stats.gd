f38cd1b60ea304073671ac71d3628142eb61a3fd
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-07 19:03:36 +0700

    Cập nhập character_stats. Thêm vào biến export max_mana và current_mana.

--- scripts/data/character_stats.gd
+++ scripts/data/character_stats.gd
@@ -3,9 +3,15 @@ extends Resource
 class_name CharacterStats
 
 @export var max_health: float = 100.0
+@export var max_mana: float = 100.0
 @export var current_health: float = 100.0 :
 	set(value):
 		current_health = clamp(value, 0, max_health)
+		emit_changed() # Thông báo khi máu thay đổi (để UI cập nhật)
+
+@export var current_mana: float = 100.0 :
+	set(value):
+		current_mana = clamp(value, 0, max_mana)
 		emit_changed() # Thông báo khi máu thay đổi (để UI cập nhật)
 
 @export var speed: float = 80.0
