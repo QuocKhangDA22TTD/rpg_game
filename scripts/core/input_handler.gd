c36d521d54e9e5a1c9a2b4b27a492b272c9d20ae
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-08 16:08:23 +0700

    Tạo script input_handler.

---
+++ scripts/core/input_handler.gd
@@ -0,0 +1,16 @@
+extends Node
+class_name InputHandler
+
+signal primary_action_pressed
+signal interact_pressed
+signal skill_primary_pressed
+
+func _process(delta: float):
+	if Input.is_action_just_pressed("primary_action"):
+		primary_action_pressed.emit()
+	
+	if Input.is_action_just_pressed("interact"):
+		interact_pressed.emit()
+	
+	if Input.is_action_just_pressed("skill_primary"):
+		skill_primary_pressed.emit()
