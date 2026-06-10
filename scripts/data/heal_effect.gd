63f753ccb1a9122af9dd1945f6c644fc8ffc4f68
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-10 17:22:49 +0700

    Đổi tên tham số player thành entity và node là Node2D.

--- scripts/data/heal_effect.gd
+++ scripts/data/heal_effect.gd
@@ -3,11 +3,11 @@ class_name HealEffect
 
 @export var heal_amount: int = 80 # Lượng máu được hồi khi sử dụng bình hồi máu
 
-func apply_effect(player: CharacterBody2D) -> bool:
-	if player.stats.current_health >= player.stats.max_health:
+func apply_effect(entity: Node2D) -> bool:
+	if entity.stats.current_health >= entity.stats.max_health:
 		return false # Nếu máu đã đầy thì không cho sử dụng bình hồi máu
 	
 	# Hồi máu nhưng không vượt quá máu tối đa
-	player.stats.current_health = min(player.stats.current_health + heal_amount, player.stats.max_health)
+	entity.stats.current_health = min(entity.stats.current_health + heal_amount, entity.stats.max_health)
 
 	return true # Sử dụng thành công
