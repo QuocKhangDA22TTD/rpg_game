e7ab7519ada0c249e703f0b9808492a7839820b7
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-17 18:45:30 +0700

    Cập nhập script item_data, thêm vào biến export price.

--- scripts/data/item_data.gd
+++ scripts/data/item_data.gd
@@ -2,16 +2,12 @@
 extends Resource
 class_name ItemData
 
-# ID duy nhất của vật phẩm
-@export var id : String
-# Tên hiển thị của vật phẩm
-@export var name : String
-# Icon hiển thị trong UI
-@export var icon : Texture2D
-# Số lượng tối đa có thể xếp chồng trong 1 slot
-@export var max_stack : int = 99
-# Tỷ lệ kích thước khi hiển thị trên bản đồ
-@export var world_scale : float = 0.0
+@export var id : String # ID duy nhất của vật phẩm
+@export var name : String # Tên hiển thị của vật phẩm
+@export var icon : Texture2D # Icon hiển thị trong UI
+@export var max_stack : int = 99 # Số lượng tối đa có thể xếp chồng trong 1 slot
+@export var world_scale : float = 0. # Tỷ lệ kích thước khi hiển thị trên bản đồ
+@export var price: int = 10
 
 # Enum định nghĩa các loại vật phẩm
 enum ItemType {
@@ -21,5 +17,4 @@ enum ItemType {
 	ARMOR        # Giáp
 	}
 
-# Loại vật phẩm
-@export var type : ItemType
+@export var type : ItemType # Loại vật phẩm
