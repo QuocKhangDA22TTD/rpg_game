4fba6390ebf80232da6fb1192248931c9a669761
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-17 19:17:22 +0700

    Cập nhập autoload game_manager, thêm vào biến export shop_ui để tham chiếu tới instance của scene shop_ui.

--- scripts/core/game_manager.gd
+++ scripts/core/game_manager.gd
@@ -4,6 +4,7 @@ var player # Tham chiếu đến player
 var enemies_container # Tham chiếu đến container chứa các enemy
 var input_handler # Tham chiếu đến input handler để xử lý input
 var hotbar # Tham chiếu đến hotbar UI
+var shop_ui # Tham chiếu đến shop UI
 
 func _ready():
 	pass
