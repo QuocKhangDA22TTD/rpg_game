cdbcd23825ecbb2c13816efbcbc9765d800732c5
QuocKhangDA22TTD <quoc.khang.12a4@gmail.com>
2026-06-18 23:08:06 +0700

    Cập nhập script game và scene game: - Tạo 2 biến export là maps_container và entities_container trong script game để tham chiếu tới node chứa map và node chứa thực thể. - Gán vào các biến đã tạo trên vào các biên export bên GameManager để tham chiếu.

--- scripts/core/game.gd
+++ scripts/core/game.gd
@@ -4,6 +4,8 @@ extends Node2D
 @export var input_handler: InputHandler # Tham chiếu đến input handler để xử lý input
 @export var hotbar_ui: Control # Tham chiếu đến hotbar UI
 @export var shop_ui: Control # Tham chiếu đến hotbar UI
+@export var maps_container: Node2D # Tham chiếu đến container chứa map
+@export var entities_container: Node2D # Tham chiếu đến container chứa các entity
 
 # Hàm sẽ được gọi 1 lần khi scene đã load xong tất cả node.
 func _enter_tree() -> void:
@@ -11,3 +13,5 @@ func _enter_tree() -> void:
 	GameManager.input_handler = input_handler # gán input handler cho biến input_handler của GameManager
 	GameManager.hotbar = hotbar_ui # gán hotbar UI cho biến hotbar của GameManager
 	GameManager.shop_ui = shop_ui # gán shop UI cho biến shop_ui của GameManager
+	GameManager.maps_container = maps_container # gán container chứa map cho biến maps_container của GameManager
+	GameManager.entities_container = entities_container # gán container chứa entity cho biến entities_container của GameManager
